2edb146398cffae7a93388a83fefcfc541be8c49
haster12345 <tarunmankani25@gmail.com>
2025-04-28 17:36:15 +0100

    get requests for tasks

--- src/app.py
+++ src/app.py
@@ -1,26 +1,38 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+from db import DB
 
 app = Flask(__name__)
 CORS(app)
 
-tasks = [
-    {
-        "id":"1",
-        "desc": "KINGS PS"
-     }
-]
+db = DB()
 
 @app.route("/tasks")
 def get_tasks():
-    return jsonify(tasks)
+    tasks = db.get_tasks()
+    res = jsonify(tasks)
+    return res
 
 @app.route("/tasks", methods=["POST"])
 def create_task():
-    tasks.append(request.get_json())
+    db.create_tasks(request.get_json())
     return "", 204
 
+@app.route("/change/id", methods=["PATCH"])
+def change_task_id(id):
+    pass
+
+@app.route("/change/casenum", methods=["PATCH"])
+def change_task_casenum(id):
+    pass
+
+@app.route("/change/description", methods=["PATCH"])
+def change_task_desc(id):
+    pass
+
+@app.route("/change/status", methods=["PATCH"])
+def change_task_status():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/db.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+
+class DB:
+    def __init__(self):
+        self.db: str = "tasks_dev.db"
+        self._create_tables()
+
+    def _create_tables(self) -> None:
+        con = sqlite3.connect(self.db)
+        with con:
+            con.executescript("""
+                BEGIN;
+                CREATE TABLE IF NOT EXISTS tasks(
+                    id integer primary key autoincrement, 
+                    CaseNumber INT,
+                    title TEXT,
+                    description TEXT, 
+                    status TEXT, 
+                    CreatedDate TEXT);
+                COMMIT;
+                """)
+        return
+
+    def get_tasks(self) -> list[dict]:
+        con = sqlite3.connect(self.db)
+        con.row_factory = sqlite3.Row
+        with con:
+            res = con.execute(
+                """
+                SELECT * FROM tasks;
+                """
+            )
+            return [dict(row) for row in res.fetchall()]
+
+    def create_tasks(self, data):
+        con = sqlite3.connect(self.db)
+        with con:
+            con.execute(
+                """
+                insert into tasks
+                values(:id, :CaseNumber, :title, :description, :status, :CreatedDate);
+                """,
+                data
+            )
+        return
+
+    def update_task_case_number(self, id, case_num):
+        con = sqlite3.connect(self.db)
+        with con:
+            con.execute(
+                """
+                update tasks
+                set CaseNumber = :case_num 
+                where id = :id;
+                """,
+                {
+                    'id': id,
+                    'case_num': case_num
+                }
+            )
+        return
+
+
